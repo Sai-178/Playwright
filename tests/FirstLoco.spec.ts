f108e638deb12a19a2787c1c5b1c82017f145f1f
Rasaabathuni-Sai-Narendra <rasaabathuni.sai.narendra@ibm.com>
2026-06-13 19:44:14 +0530

    3rd committ

--- tests/FirstLoco.spec.ts
+++ tests/FirstLoco.spec.ts
@@ -1,12 +1,47 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test.skip('test', async ({ page }) => {
   await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
   await page.getByRole('textbox', { name: 'Username:' }).click();
-  await page.getByRole('textbox', { name: 'Username:' }).fill('sai_158');
+  await page.getByRole('textbox', { name: 'Username:' }).fill('rahulshettyacademy');
   await page.getByRole('textbox', { name: 'Password:' }).click();
-  await page.getByRole('textbox', { name: 'Password:' }).fill('Narendra');
+  await page.getByRole('textbox', { name: 'Password:' }).fill('Learning@830$3mK2');
   await page.getByRole('combobox').selectOption('consult');
   await page.getByRole('checkbox', { name: 'I Agree to the terms and' }).check();
   await page.getByRole('button', { name: 'Sign In' }).click();
+});
+test.skip("loginpage using css selectors",async({page})=>{
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  await page.locator('input#username').click(); 
+  await page.locator("input#username").fill("rahulshettyacademy");
+  await page.locator('input#password').click();
+  await page.locator("input#password").fill("Learning@830$3mK2");
+   
+
+});
+
+test.skip("logging failed and extract the failed messaage",async({page})=>{
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  await page.locator('input#username').click();
+  await page.locator("input#username").fill("rahulshettyacademy");
+  await page.locator('input#password').click();
+  await page.locator("input#password").fill("Learning@830$3mK");
+  await page.locator('input#signInBtn').click();
+  //console.log(await page.locator('[style*="block"]').textContent());
+  await expect( await page.locator('[style*="block"]')).toContainText('Incorrect username/password.');
+});
+test.only("grab the title of the the first product",async({page})=>{
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  await page.locator('input#username').click();
+  await page.locator("input#username").fill("rahulshettyacademy");
+  await page.locator('input#password').click();
+  await page.locator("input#password").fill("Learning@830$3mK2");
+  await page.locator('input#signInBtn').click();
+  //console.log(await page.locator('[style*="block"]').textContent());
+ // await expect( await page.locator('[style*="block"]')).toContainText('Incorrect username/password.');
+  console.log(await page.locator('.card-title a').nth(1).textContent());
+  await expect(await page.locator(".card-title a").nth(1)).toContainText("Samsung Note 8");
+  console.log(await page.locator(".card-title a").first().textContent());
+  const alltitles=await page.locator(".card-title a").allTextContents();
+  console.log(alltitles);
 });
